7e476af835414ac5dd84ce1dd383fbaed621adb0
Ganga Jathin <gganga.jathin.bce17@itbhu.ac.in>
2017-12-11 13:58:43 +0530

    my commit

---
+++ NEXUS.py
@@ -0,0 +1,199 @@
+def title():
+    print("THE.......")
+    print("MYSTERY.....")
+    print("press p to play, q to exit....")
+    print("P.S......press enter to advance to next message")
+    
+title()
+
+print("you are in a cold dark room with an old man with a scar on his eye beside you")
+print("All you remember is that few minutes ago u were happy ")
+print("playing with your friends and then suddenly......")
+
+print(" THRASH:!!!    hey you what's your name kiddo!")
+print("Enter Your Name")                                                                                                                                                                                                                                                                                             
+  
+name = input("") 
+print("ok" +  name  + "listen you wanna know why you are here??") 
+print("y(yes)/n(no)")
+ch=input()
+if ch=="y":
+    print("THEN ! do what i say i will tell you everything after you help me")
+    input()
+    print("......oh no....")
+    input()
+    print("he is coming shoot him with the gun behind you!!!!")
+else:
+    print("what an idiotic question i asked......")
+    input()
+    print("help me you dont have any other choice or else you will die ......")
+    input()
+    print("He is coming shoot him with the gun behind you!!!!!")
+     
+
+print("s(shoot)/d(not shoot)")
+ch=input()
+if ch=="s":
+    print("hey kid nice shot dude! well we got the keys there lets get out")
+    input()
+    print(" well you dont know but you just became the underwold biggest mafia don")
+    input()
+    print("can't believe it! welcome to mafiaworld your majesty")  
+   
+ 
+
+else:
+    print("the intruder takes a gun and kills u")
+    input()
+    print("GAME OVER GAME OVER GAME OVER GAME OVER")
+input()
+title()
+print("CHAPTER TWO........")
+input()
+print("so u wanna know your parents flashback huh?")
+print("y(yes)/n(no)")
+ch=input()
+if ch=="y":
+    print("your father was mafia king of the golden mafia era")
+    input()
+    print("after his death you are the next mafia king")
+    input()
+    print("you will take the responsibility, won't you?")
+    input()
+    
+    print("a] 'of course i want to become a king why not for mafias'")
+    print("b] 'what is my dad that scary, no i will call police to take care of the rest'")
+    print("c] 'nah, not interested in these stuffs...booring ....lame....'")
+    print("a/b/c")
+    ch=input()
+    if ch=="a":
+        print("i know his blood runs through your veins well then i shall be your minister")
+        print("as a first step kill this innocent young man will you")
+        print("k(kill)/n(no kill)")
+        ch=input()         
+        if ch=="k":
+            print("Very well, this world shall be ours!")
+        else:
+            print("what a innocent kid you are i need to mend your ways first")
+    elif ch=="b":
+        print("HOW THE HELL CAN YOU BE SO ARROGANT TO YOUR DAD OUR KING I NEED TO KILL YOU HERE NOW")
+        input()
+        print("takes the gun and kills you")
+        input()
+        print("GAME OVER GAME OVER GAME OVER")     
+    else:
+        print("impossible his blood should run through your veins")
+        input()
+        print("well as a witness of the truth i must kill you now am sorry")
+        input()
+        print("takes the gun annd kills you")
+        input()
+        print("GAME OVER GAME OVER GAME OVER")
+    
+else:
+    print("you must know your parents flashback and then decide your fate ok dude!")
+    input()
+    print("your father was mafia king of the golden mafia era")
+    input()
+    print("after his death you are the next mafia king")
+    input()
+    print("you will take the responsibility, won't you?")
+    input()
+    
+    print("a] 'of course i want to become a king why not for mafias'")
+    print("b] 'what is my dad that scary, no i will call police to take care of the rest'")
+    print("c] 'nah, not interested in these stuffs...booring ....lame....'")
+    print("a/b/c")
+    ch=input()
+    if ch=="a":
+        print("i know his blood runs through your veins well then i shall be your minister")
+        print("as a first step kill this innocent young man will you")
+        print("k(kill)/n(no kill)")
+        ch=input()         
+        if ch=="k":
+            print("Very well, this world shall be ours!")
+        else:
+            print("what a innocent kid you are i need to mend your ways first")
+    elif ch=="b":
+        print("HOW THE HELL CAN YOU BE SO ARROGANT TO YOUR DAD OUR KING I NEED TO KILL YOU HERE NOW")
+        input()
+        print("takes the gun and kills you")
+        input()
+        print("GAME OVER GAME OVER GAME OVER")     
+    else:
+        print("impossible his blood should run through your veins")
+        input()
+        print("well as a witness of the truth i must kill you now am sorry")
+        input()
+        print("takes the gun annd kills you")
+        input()
+        print("GAME OVER GAME OVER GAME OVER")
+input()
+print("thanks for playing this game its name is MAFIUS")
+title()
+print("CHAPTER TWO........")
+input()
+print("so u wanna know your parents flashback huh?")
+print("y(yes)/n(no)")
+ch=input()
+if ch=="y":
+    print("your father was mafia king of the golden mafia era")
+    input()
+    print("after his death you are the next mafia king")
+    input()
+    print("you will take the responsibility, won't you?")
+    input()
+    
+    print("a] 'of course i want to become a king why not for mafias'")
+    print("b] 'what is my dad that scary, no i will call police to take care of the rest'")
+    print("c] 'nah, not interested in these stuffs...booring ....lame....'")
+    print("a/b/c")
+    ch=input()
+    if ch=="a":
+        print("i know his blood runs through your veins well then i shall be your minister")
+        print("as a first step kill this innocent young man will you")
+        print("k(kill)/n(no kill)")
+        ch=input()         
+        if ch=="k":
+            print("Very well, this world shall be ours!")
+        else:
+            print("what a innocent kid you are i need to mend your ways first")
+    elif ch=="b":
+        print("HOW THE HELL CAN YOU BE SO ARROGANT TO YOUR DAD OUR KING I NEED TO KILL YOU HERE NOW")
+        input()
+        print("takes the gun and kills you")
+        input()
+        print("GAME OVER GAME OVER GAME OVER")     
+    else:
+        print("impossible his blood should run through your veins")
+        input()
+        print("well as a witness of the truth i must kill you now am sorry")
+        input()
+        print("takes the gun annd kills you")
+        input()
+        print("GAME OVER GAME OVER GAME OVER")
+    
+
+    
+
+
+    
+
+
+          
+ 
+    
+
+
+   
+   
+    
+
+     
+
+
+        
+    
+    
+
+
